997858e9c950e7556cdaf3a311abda955d5bd2ff
Max W <mwinter@wesleyan.edu>
2021-11-10 14:42:09 -0600

    added searching ability by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -18,6 +19,20 @@ var formSubmitHandler = function(event) {
     console.log(event);
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert("Error: GitHub user not found");
+        }
+    });
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -88,5 +103,14 @@ var displayRepos = function(repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+};
+
 console.log("outside");
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
